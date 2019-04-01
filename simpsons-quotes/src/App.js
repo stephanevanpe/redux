a52c447ff685f84fd7c094ee9aea05aa96254ea6
Stephane Van Pe <stephanevanpe@gmail.com>
2019-04-01 11:47:26 +0200

    Creation componnent

--- simpsons-quotes/src/App.js
+++ simpsons-quotes/src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 import Quote from "./Quote";
+import Quotes from "./Quotes";
 
 class App extends Component {
   render() {
@@ -17,11 +18,13 @@ class App extends Component {
           character="Homer Simpson"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939"
         />
+
         <Quote
           quote="Me fail English? That's unpossible"
           character="Ralph Wiggum"
           image="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FRalphWiggum.png?1497567511523"
         />
+        <Quotes/>
       </div>
     );
   }
